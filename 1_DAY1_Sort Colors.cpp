700cb751074f73b506223b4f62b81793dc1d076f
Prince Choudhury <99341924+Prince-Choudhury@users.noreply.github.com>
2023-11-01 20:51:21 +0530

    Sort Colors

---
+++ 1_DAY1_Sort Colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int red = 0, white = 0, blue = 0;
+        for(int i = 0; i<nums.size();i++){
+            if(nums[i] ==0)
+                red++;
+            else if(nums[i] == 1)
+                white++;
+            else
+                blue++;
+        }
+        int i = 0;
+        while(red--){
+            nums[i] = 0;
+            i++;
+        }
+        while(white--){
+            nums[i] = 1;
+            i++;
+        }
+        while(blue--){
+            nums[i] = 2;
+            i++;
+        }  
+    }
+};
