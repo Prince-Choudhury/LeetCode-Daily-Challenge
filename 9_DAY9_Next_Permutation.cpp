964ca0027b46249a56e06697b78c211fb5651fce
Prince Choudhury <99341924+Prince-Choudhury@users.noreply.github.com>
2023-11-09 23:08:15 +0530

    Next Permutation

---
+++ 9_DAY9_Next_Permutation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size(); 
+        int index = -1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        for(int i=n-1; i>=index && index != -1; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin() + index + 1, nums.end());
+    }
+};
