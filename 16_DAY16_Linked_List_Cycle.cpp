b1eb2dc143b70c4997f8c6ae926aac7cd7560f35
Prince Choudhury <choudhuryprince19@gmail.com>
2023-11-16 20:51:09 +0530

    Linked List Cycle

---
+++ 16_DAY16_Linked_List_Cycle.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        map<ListNode*, bool> table;
+
+        ListNode* temp = head;
+
+        while(temp != NULL){
+            if(table[temp] == false){
+                table[temp] = true;
+            }
+
+            else{
+                //cycle present
+                return true;
+            }
+            temp = temp -> next;
+        }
+        //cycle not present
+        return false;
+    }
+};
