6b5a7fe7b534abae28f9033a1be12314bbcba02b
Prince Choudhury <99341924+Prince-Choudhury@users.noreply.github.com>
2023-12-07 19:31:11 +0530

    Implement Stack Using Queues

---
+++ 37_DAY37_Implement_Stack_Using_Queues.cpp
@@ -0,0 +1,50 @@
+class MyQueue {
+private:
+    stack<int> inputStack;
+    stack<int> outputStack;
+
+public:
+    MyQueue() {}
+
+    void push(int x) {
+        inputStack.push(x);
+    }
+
+    int pop() {
+        if (outputStack.empty()) {
+            while (!inputStack.empty()) {
+                outputStack.push(inputStack.top());
+                inputStack.pop();
+            }
+        }
+
+        int front = outputStack.top();
+        outputStack.pop();
+        return front;
+    }
+
+    int peek() {
+        if (outputStack.empty()) {
+            while (!inputStack.empty()) {
+                outputStack.push(inputStack.top());
+                inputStack.pop();
+            }
+        }
+
+        return outputStack.top();
+    }
+
+    bool empty() {
+        return inputStack.empty() && outputStack.empty();
+    }
+};
+
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
