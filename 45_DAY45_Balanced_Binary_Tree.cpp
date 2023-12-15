1930d8daee9492cd8d80c7d870e30ae9c1fc9121
Prince Choudhury <99341924+Prince-Choudhury@users.noreply.github.com>
2023-12-15 21:01:46 +0530

    Balanced Binary Tree.

---
+++ 45_DAY45_Balanced_Binary_Tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isbalanced = true;
+    int height(TreeNode* root){
+        if(!root){
+            return 0;
+        }
+
+        int lh = height(root->left);
+        int rh = height(root->right);
+
+        //check for current node, is it balanced?
+        if(isbalanced && abs(lh - rh) > 1){
+            isbalanced = false;
+        }
+
+        return max(lh, rh) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        height(root);
+        return isbalanced;
+    }
+};
