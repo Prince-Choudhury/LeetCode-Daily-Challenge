ec862b40a292d212fe113ab000e34b2b73ccbc29
Prince Choudhury <99341924+Prince-Choudhury@users.noreply.github.com>
2024-01-29 22:44:14 +0530

    Minimum Add to Make Parentheses Valid

---
+++ 90_DAY90_Min_Add_To_Make_Parentheses_Valid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int ans = 0;
+        for(auto ch:s){
+            if(ch == '('){
+                ans++;
+                st.push(ch);
+            }
+            else{
+                if(!st.empty()){
+                    st.pop();
+                    ans--;
+                }
+                else{
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
