89f091294a7b404a509cd76d9268d5f5e1e7fddf
Prince Choudhury <99341924+Prince-Choudhury@users.noreply.github.com>
2024-01-30 22:28:35 +0530

    Find the Minimum and Maximum Number of Nodes Between Critical Points

---
+++ 91_DAY91_Find_Min_and_Max_Num_of_Nodes_Between_Critical_Points.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int>ans = {-1, -1}; // minDist, maxDist
+        ListNode* prev = head;
+        if(!prev){
+            return ans;
+        }
+        ListNode* curr = head->next;
+        if(!curr){
+            return ans;
+        }
+        ListNode* nxt = head->next->next;
+        if(!nxt){
+            return ans;
+        }
+
+        int firstCP = -1;
+        int lastCP = -1;
+        int minDist = INT_MAX;
+        int i = 1;
+        while(nxt){
+            bool isCP = ((curr->val > prev->val && curr->val > nxt->val) || (curr->val < prev->val && curr->val < nxt->val)) ? true : false;
+
+            if(isCP && firstCP == -1){
+                firstCP = i;
+                lastCP = i;
+            }
+            else if(isCP){
+                minDist = min(minDist, i - lastCP);
+                lastCP = i;
+            }
+            i++;
+            prev = prev->next;
+            curr = curr->next;
+            nxt = nxt->next;
+
+        }
+        if(lastCP == firstCP){
+            //Only 1 CP was found
+            return ans;
+        }
+        else{
+            ans[0] = minDist;
+            ans[1] = lastCP - firstCP;
+        }
+        return ans;
+    }
+};
