5b8de2c63ecfa3a2192257891397d2f769747101
Prince Choudhury <choudhuryprince19@gmail.com>
2025-01-17 08:27:46 +0530

    Time: 3 ms (59.81%), Space: 264 MB (28.04%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& d) {
+       int m=0;
+        for(auto x:d)
+            m^=x;
+        if(m==0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
