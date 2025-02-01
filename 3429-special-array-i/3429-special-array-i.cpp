915428ca7b57ff181f87bba32ce9aea09861d7e1
Prince Choudhury <choudhuryprince19@gmail.com>
2025-02-01 09:25:35 +0530

    Time: 0 ms (100%), Space: 27.7 MB (61.71%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    void solve(vector<int>&arr, vector<pair<int, int>>&p){
-        int n = arr.size();
-        for(int i = 0; i<n; i++){
-            if(i+1<n){
-                p.push_back(make_pair(arr[i], arr[i+1]));
-            }
-            
-        }
-    }
     bool isArraySpecial(vector<int>& nums) {
         if(nums.size() == 0 || nums.size() == 1){
             return true;
         }
 
-        vector<pair<int, int>>p;
-        solve(nums, p);
-
-        for(auto i : p){
-            if((i.first & 1) && (i.second & 1)){
+        for(int i = 0; i<nums.size()-1; i++){
+            int ele1 = nums[i];
+            int ele2 = nums[i+1];
+            if(((ele1 & 1) && (ele2 & 1)) || ((ele1 % 2 == 0) && (ele2 % 2 == 0))){
                 return false;
-            }
-
-            if((i.first % 2 == 0) && (i.second % 2 == 0)){
-                return false;
+                break;
             }
 
         }
