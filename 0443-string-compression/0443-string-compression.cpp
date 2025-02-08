088e754f51b4ea25518dd066308921b543333298
Prince Choudhury <choudhuryprince19@gmail.com>
2025-02-08 14:57:49 +0530

    Time: 1 ms (21.77%), Space: 13.7 MB (73.77%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int compress(vector<char>& s) {
+        string ans = "";
+        int i = 0, n = s.size();
+        while (i < n) {
+            int count = 1;
+            char ch = s[i];
+            
+            if (i+1<n && s[i] == s[i + 1]) {
+                while (i +1< n && s[i] == s[i + 1]) {
+                    count++;
+                    i++;
+                }
+                
+                if (count > 1) {
+                    string temp = to_string(count);
+                    ans += s[i];
+                    ans += temp;
+                    i=i+1;
+                }
+            }else{
+                ans+=s[i];
+                i++;
+            }
+        }
+        s.clear();
+
+        for(int i=0;i<ans.size();i++)
+        {
+            char ch = ans[i];
+            s.push_back(ch);
+        }
+        
+        return s.size();
+    }
+};
