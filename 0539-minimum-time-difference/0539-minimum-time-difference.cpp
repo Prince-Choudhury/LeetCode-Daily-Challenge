b375cd6ddc314e60f168d7afae40b94997b63583
Prince Choudhury <choudhuryprince19@gmail.com>
2025-02-09 08:19:02 +0530

    Time: 2 ms (79.2%), Space: 18.1 MB (54.58%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,32 +1,5 @@
 class Solution {
 public:
-    int convert(string s){
-        int num = 0;
-        char ch1 = s[0];
-        char ch2 = s[1];
-
-        int n1 = ch1 - 'a';
-        int n2 = ch2 - 'a';
-
-        num = num + n1;
-        num = num * 10 + n2;
-        num = num * 60;
-
-        char ch3 = s[3];
-        char ch4 = s[4];
-        
-        int n3 = ch3 - 'a';
-        int n4 = ch4 - 'a';
-
-        int num2 = 0;
-        num2 = num2 + n3;
-        num2 = num2* 10 + n4;
-
-        int ans = num + num2;
-
-        return ans;
-    }
-
     int solve(int a, int b){
 
         if(a == b){
@@ -54,7 +27,13 @@ public:
         vector<int>v;
 
         for(int i = 0; i<n; i++){
-            int minutes = convert(arr[i]);
+            string s = arr[i];
+            int h = stoi(s.substr(0, 2));
+            h = h*60;
+
+            int m = stoi(s.substr(3, 2));
+
+            int minutes = h + m;
 
             v.push_back(minutes);
         }
