e601c05332f170b285849675ceb3d6f845658355
Prince Choudhury <choudhuryprince19@gmail.com>
2025-02-09 09:28:53 +0530

    Time: 0 ms (100%), Space: 13.8 MB (18.04%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    int compress(vector<char>& s) {
+    int compress(vector<char>& chars) {
         string ans = "";
-        int i = 0, n = s.size();
-        while (i < n) {
+        int n = chars.size();
+        int i = 0;
+
+        while(i<n){
             int count = 1;
-            char ch = s[i];
-            
-            if (i+1<n && s[i] == s[i + 1]) {
-                while (i +1< n && s[i] == s[i + 1]) {
+            char ch = chars[i];
+
+            if(i+1<n && chars[i] == chars[i+1]){
+                while(i+1<n && chars[i] == chars[i+1]){
                     count++;
                     i++;
                 }
-                
-                if (count > 1) {
-                    string temp = to_string(count);
-                    ans += ch;
-                    ans += temp;
-                    i=i+1;
+
+                if(count>1){
+                    string s = to_string(count);
+                    ans+=ch;
+                    ans+= s;
+                    i++;
                 }
-            }else{
+            }
+            else{
                 ans+=ch;
                 i++;
             }
         }
-        s.clear();
 
-        for(int i=0;i<ans.size();i++)
-        {
-            char ch = ans[i];
-            s.push_back(ch);
+        chars.clear();
+        for(int i =0; i<ans.size(); i++){
+            char ch1 = ans[i];
+            chars.push_back(ch1);
         }
-        
-        return s.size();
+
+        return ans.size();
     }
 };
