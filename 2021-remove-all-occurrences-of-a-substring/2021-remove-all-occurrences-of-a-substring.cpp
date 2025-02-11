3002b309ec00bbef077ce8259c651bfba58cdac2
Prince Choudhury <choudhuryprince19@gmail.com>
2025-02-11 10:21:38 +0530

    Time: 7 ms (5.85%), Space: 13.6 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,81 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        while(s.find(part)!=string::npos){
-            s.erase(s.find(part), part.size());
+    vector<int> computeLPS(string s){
+        vector<int>lps(s.size(), 0);
+        int i = 1;
+        int j = 0;
+        while(i<s.size()){
+            if(s[i]==s[j]){
+                lps[i] = j+1;;
+                i++;
+                j++;
+            }
+
+            else{
+                if(j == 0) i++;
+                else j = lps[j-1];
+            }
         }
 
+        return lps;
+    }
+
+    bool applyKMP(string s, string part){
+        vector<int> lps = computeLPS(part);
+
+        int i = 0;
+        int j = 0;
+        while(i<s.size()){
+            if(s[i] == part[j]){
+                i++;
+                j++;
+            }
+
+            else{
+                if(j == 0) i++;
+                else j = lps[j-1];
+            }
+
+            if(j == part.size()){
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    int findIndex(string s, string part){
+        vector<int> lps = computeLPS(part);
+
+        int i = 0;
+        int j = 0;
+        int index = -1;
+        while(i<s.size()){
+            if(s[i] == part[j]){
+                i++;
+                j++;
+            }
+
+            else{
+                if(j == 0) i++;
+                else j = lps[j-1];
+            }
+
+            if(j == part.size()){
+                index = (i-1) - part.size() + 1; // i-part.size()
+                return index;
+            }
+        }
+
+        return -1;
+    }
+
+    string removeOccurrences(string s, string part) {
+            while(applyKMP(s, part)){
+                int index = findIndex(s, part);
+                s.erase(index, part.size());
+            }
+        
         return s;
     }
 };
