73f605e61b8240c6c250b909ccb14d46fa90159f
Prince Choudhury <choudhuryprince19@gmail.com>
2025-02-11 10:25:16 +0530

    Time: 1 ms (50.91%), Space: 9.6 MB (13.98%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,88 +1,9 @@
 class Solution {
 public:
-    vector<int> computeLPS(string s){
-        vector<int>lps(s.size(), 0);
-        int i = 1;
-        int j = 0;
-        while(i<s.size()){
-            if(s[i]==s[j]){
-                lps[i] = j+1;;
-                i++;
-                j++;
-            }
-
-            else{
-                if(j == 0) i++;
-                else j = lps[j-1];
-            }
-        }
-
-        return lps;
-    }
-
-    bool applyKMP(string s, string part){
-        vector<int> lps = computeLPS(part);
-
-        int i = 0;
-        int j = 0;
-        while(i<s.size()){
-            if(s[i] == part[j]){
-                i++;
-                j++;
-            }
-
-            else{
-                if(j == 0) i++;
-                else j = lps[j-1];
-            }
-
-            if(j == part.size()){
-                return 1;
-            }
-        }
-
-        return 0;
-    }
-
-    int findIndex(string s, string part){
-        vector<int> lps = computeLPS(part);
-
-        int i = 0;
-        int j = 0;
-        int index = -1;
-        while(i<s.size()){
-            if(s[i] == part[j]){
-                i++;
-                j++;
-            }
-
-            else{
-                if(j == 0) i++;
-                else j = lps[j-1];
-            }
-
-            if(j == part.size()){
-                index = (i-1) - part.size() + 1; // i-part.size()
-                return index;
-            }
-        }
-
-        return -1;
-    }
-    void solve(string &s, string part)
-    {
-        if(!applyKMP(s,part))
-        {
-            return;
-        }else{
-            int index =findIndex(s,part);
-            s.erase(index,part.size());
-            solve(s,part);
-        }
-    }
     string removeOccurrences(string s, string part) {
-        solve(s,part);
-        
+        while(s.length()>0 && s.length()>s.find(part)){
+           s.erase(s.find(part),part.length());
+        }
         return s;
     }
 };
