483ed55bec924b50444e46744f528093378b69cf
Prince Choudhury <choudhuryprince19@gmail.com>
2025-03-04 17:21:13 +0530

    Time: 6 ms (12.21%), Space: 8.1 MB (9.62%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,10 +1,39 @@
 class Solution {
 public:
-    bool checkPowersOfThree(int n) {
-        if(n==1)
+    bool findSubsequence(vector<int>& nums, int index, int sum, int k) {
+        // If sum becomes k, return true
+        if (sum == k) return true;
+
+        // Base Case: If we reach the end, return false
+        if (index == nums.size()) return false;
+
+        // Include current element
+        if (findSubsequence(nums, index + 1, sum + nums[index], k)) 
             return true;
-        if(n%3==2)
-            return false;
-        return checkPowersOfThree(n/3);
+
+        // Exclude current element
+        if (findSubsequence(nums, index + 1, sum, k)) 
+            return true;
+
+        return false;
+    }
+    
+    bool checkPowersOfThree(int n) {
+        vector<int> arr;
+        bool run =true;
+        int mul = 1;
+        while(run)
+        {
+            if(mul>n)
+            {
+                run = false;
+            }else{
+                arr.push_back(mul);
+                mul = mul*3;
+            }
+        }
+        bool count = findSubsequence(arr,0, 0, n);
+        return count;
+       
     }
 };
