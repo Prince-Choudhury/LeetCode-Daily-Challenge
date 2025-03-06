92c71832267b84a7677bcb881be97505310ab552
Prince Choudhury <choudhuryprince19@gmail.com>
2025-03-06 11:33:15 +0530

    Time: 15 ms (49.2%), Space: 31.7 MB (24.71%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int>arr;
-        for(int i = 0; i<grid.size(); i++){
-            for(int j = 0; j<grid.size(); j++){
-                arr.push_back(grid[i][j]);
+        unordered_map<int, int> mp;
+        int n = grid.size();
+        int a = 0, b = 0;
+        long long sum = 0;
+        long long x = n * n;
+        long long actual_sum = x * (x + 1) / 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int ele = grid[i][j];
+                sum += ele;
+                if (mp.find(ele) != mp.end()) {
+                    a = ele;
+                    mp[ele]++;
+                } else {
+                    mp[ele]++;
+                }
             }
         }
-
-        vector<int>ans;
-
-        sort(arr.begin(), arr.end());
-
-        for(int i = 0; i<arr.size()-1; i++){
-            if(arr[i] == arr[i+1]){
-                ans.push_back(arr[i]);
-            }
-        }
-
-        bool isTrue = false;
-        for(int i = 0; i<arr.size()-1; i++){
-            if(abs(arr[i] - arr[i+1]) == 2){
-                isTrue = true;
-                ans.push_back(arr[i]+1);
-            }
-        }
-
-        int n = arr.size();
-        int m = grid.size();
-
-        if(isTrue == false && arr[n-1] != m*m){
-            ans.push_back(arr[arr.size()-1]+1);
-        }
-        
-        if(isTrue == false && arr[n-1] == m*m){
-            ans.push_back(1);
-        }
-
+        sum = sum - a;
+        b = actual_sum - sum;
+        vector<int> ans;
+        ans.push_back(a);
+        ans.push_back(b);
         return ans;
     }
 };
