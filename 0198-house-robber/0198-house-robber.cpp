f37630b996ae097462170fc92049eb34d5f10b10
Prince Choudhury <choudhuryprince19@gmail.com>
2025-03-26 16:55:21 +0530

    Time: 2 ms (7.42%), Space: 10.7 MB (21.8%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 
     /*
+    //1. Using Recursion
     int solve(vector<int>arr, int n){
         if(n<0){
             return 0;
@@ -16,6 +17,11 @@ public:
         return max(incAns, excAns);
     }*/
 
+
+
+    /*
+
+    //Uisng Recursion + Memo.
     int solveUsingMem(vector<int> arr, vector<int>&dp, int index){
         if(index>=arr.size()){
             return 0;
@@ -35,12 +41,40 @@ public:
 
         return dp[index];
 
+    }*/
+
+
+    //Using Tabulationg
+
+    int solve(vector<int>arr, vector<int>&dp, int n){
+        dp[n-1] = arr[n-1];
+
+        for(int index = n-2; index>=0; index--){
+            int temp = 0;
+
+            //Include
+            if(index+2<n){
+                temp = dp[index+2];
+            }
+            int incAns = arr[index] + temp;
+            
+
+            //Exclude
+            int excAns = 0 + dp[index+1];
+
+            dp[index] = max(incAns, excAns);
+
+        }
+
+        return dp[0];
     }
 
-    int rob(vector<int>& nums) {
-        
-        vector<int>dp(nums.size() + 1, -1);
 
-        return solveUsingMem(nums, dp, 0);
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n, -1);
+
+        return solve(nums, dp, n);
     }
 };
