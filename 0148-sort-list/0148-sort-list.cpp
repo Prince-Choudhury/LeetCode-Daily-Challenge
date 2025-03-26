7e9c15b41b16e113ce6ca1e890af59432dc4f83e
Prince Choudhury <choudhuryprince19@gmail.com>
2025-03-26 10:14:00 +0530

    Time: 29 ms (53.91%), Space: 66.3 MB (49.09%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -9,96 +9,44 @@
  * };
  */
 
-
-
- /*
 class Solution {
 public:
-    ListNode* solve(vector<int>&arr){
-        
-        ListNode* head = new ListNode(-1);
+
+    ListNode* solve(ListNode* head, vector<int>arr){
+        ListNode* dummy = new ListNode(-1);
+        ListNode* node = dummy;
+
         ListNode* temp = head;
+
         for(int i = 0; i<arr.size(); i++){
-            ListNode* temp1 = new ListNode(arr[i]);
-            temp->next = temp1;
-            temp = temp1;
+            ListNode* newNode = new ListNode(arr[i]);
+            node->next = newNode;
+            node = newNode;
         }
 
-        return head->next;
-
+        return dummy->next;
     }
+
     ListNode* sortList(ListNode* head) {
+
+        if(head == NULL){
+            return NULL;
+        }
+
+        if(head->next == NULL){
+            return head;
+        }
+
         vector<int>arr;
+
         ListNode* temp = head;
-        while(temp!=NULL){
+        while(temp){
             arr.push_back(temp->val);
             temp = temp->next;
         }
 
         sort(arr.begin(), arr.end());
 
-        return solve(arr);
-
-    }
-};
-
-*/
-
-
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    void solve(priority_queue<int, vector<int>, greater<int>>& pq,
-               ListNode* temp) {
-        while (temp) {
-            int d = temp->val;
-            pq.push(d);
-            temp = temp->next;
-        }
-    }
-    void insert(ListNode*& a,
-                priority_queue<int, vector<int>, greater<int>>& pq) {
-        if (pq.empty())
-            return; // Handle empty queue case
-
-        // Initialize head if it's nullptr
-        if (a == nullptr) {
-            a = new ListNode(pq.top());
-            pq.pop();
-        }
-
-        ListNode* current = a; // Maintain a pointer to traverse the list
-
-        while (!pq.empty()) {
-            int ele = pq.top();
-            pq.pop();
-            current->next = new ListNode(ele);
-            current = current->next;
-        }
-    }
-
-    ListNode* sortList(ListNode* head) {
-        if (head == NULL) {
-            return NULL;
-        }
-        priority_queue<int, vector<int>, greater<int>> pq;
-        ListNode* temp = head;
-        solve(pq, temp);
-
-        ListNode* NewHead = new ListNode(-1);
-        insert(NewHead, pq);
-        NewHead = NewHead->next;
-
-        return NewHead;
+        return solve(head, arr);
     }
 };
