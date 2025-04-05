466b09085ef68d7ca6d58b0d9fa4636faf79808b
Prince Choudhury <choudhuryprince19@gmail.com>
2025-04-05 21:16:24 +0530

    Time: 51 ms (13.85%), Space: 31.4 MB (9.81%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,53 +1,24 @@
 class Solution {
 public:
 
-    void solve(vector<int>arr, vector<vector<int>>&ans, int index, vector<int>output){
-        if(index>=arr.size()){
-            ans.push_back(output);
-            return;
+    int solve(vector<int>arr, int index, int currXor){
+        if(index==arr.size()){
+
+            return currXor;
         }
 
         //Exclude
-        solve(arr, ans, index+1, output);
+        int exclude = solve(arr, index+1, currXor);
 
         //Include
-        int ele = arr[index];
+        int include = solve(arr, index+1, currXor^arr[index]);
 
-        output.push_back(ele);
-        solve(arr, ans, index+1, output);
+        return exclude + include;
 
-    }
-
-    void compute(vector<int>arr, int&sum){
-        if(arr.empty()){
-            sum = sum + 0;
-        }
-
-        if(arr.size() == 1){
-            sum = sum + arr[0];
-        }
-
-        int Xor = 0;
-        if(arr.size()>1){
-            for(int i = 0; i<arr.size(); i++){
-                Xor = Xor ^ arr[i];
-            }
-        }
-        
-
-        sum = sum + Xor;
     }
 
     int subsetXORSum(vector<int>& arr) {
-        vector<vector<int>>ans;
-        vector<int>output;
-        solve(arr, ans, 0, output);
 
-        int sum = 0;
-        for(int i = 0; i<ans.size(); i++){
-            compute(ans[i], sum);
-        }
-
-        return sum;
+        return solve(arr, 0, 0);
     }
 };
