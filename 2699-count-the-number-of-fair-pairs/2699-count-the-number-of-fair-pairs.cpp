62fe007f973c75f6736bf6b2a9d9509925da0b8f
Prince Choudhury <choudhuryprince19@gmail.com>
2025-04-19 21:29:40 +0530

    Time: 60 ms (39.66%), Space: 60.4 MB (38.74%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -7,13 +7,10 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n; ++i) {
-            // Find the first valid index j where nums[i] + nums[j] >= lower
             int left = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
             
-            // Find the first invalid index j where nums[i] + nums[j] > upper
             int right = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
 
-            // Add the count of valid pairs between left and right
             count += (right - left);
         }
 
