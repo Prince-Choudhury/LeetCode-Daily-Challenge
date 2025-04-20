f05ebcf905874b8c72cb7b205f67ed987167cfdf
Prince Choudhury <choudhuryprince19@gmail.com>
2025-04-20 10:35:38 +0530

    Time: 7 ms (43.64%), Space: 23.8 MB (80.23%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,27 +5,17 @@ public:
 
         vector<vector<int>>ans;
 
-        vector<int>temp = arr[0];
-
-        int a = arr[0][0];
-        int b = arr[0][1];
+        ans.push_back(arr[0]);
 
         for(int i = 1; i<arr.size(); i++){
-            int c = arr[i][0];
-            int d = arr[i][1];
 
-            if(b>=c){
-                int ele = max(b, d);
-                b = ele;
+            if(arr[i][0]>ans.back()[1]){
+                ans.push_back(arr[i]);
             }
             else{
-                ans.push_back({a, b});
-                a = c;
-                b = d;
+                ans.back()[1] = max(ans.back()[1], arr[i][1]);
             }
         }
-
-        ans.push_back({a,b});
 
         return ans;
 
