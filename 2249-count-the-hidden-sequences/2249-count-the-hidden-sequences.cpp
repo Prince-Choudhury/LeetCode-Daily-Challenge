0897ccc4e00c315af5dfcd9d44a1db33e504d2b4
Prince Choudhury <choudhuryprince19@gmail.com>
2025-04-21 21:37:07 +0530

    Time: 0 ms (100%), Space: 112.6 MB (75%) - LeetHub

---
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        long sum = 0, mini = 0, maxi = 0;
+        for(int i:differences){
+            sum += i;
+            maxi = max(maxi, sum);
+            mini = min(mini, sum);
+        }
+        return max(0L, upper - maxi - lower + mini + 1);
+    }
+};
