08a06786ed76cb39eb797c30a4e8c9f9a0182237
Prince Choudhury <choudhuryprince19@gmail.com>
2025-04-30 15:59:51 +0530

    Time: 0 ms (100%), Space: 14.3 MB (22.18%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
@@ -4,12 +4,9 @@ public:
         int ans = 0;
         for(auto i : arr){
             string s = to_string(i);
-            int count = 0;
-            for(int i = 0; i<s.size(); i++){
-                count++;
-            }
-
-            if(count%2 == 0){
+            int n = s.size();
+            
+            if(n % 2 == 0){
                 ans++;
             }
         }
