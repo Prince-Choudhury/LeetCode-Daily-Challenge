43a06f138feec9b0a3a204750253e4d1cd6dd6b0
Prince Choudhury <choudhuryprince19@gmail.com>
2025-07-01 23:19:25 +0530

    Time: 3 ms (36.41%), Space: 8.9 MB (60.22%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int ans=0;
+
+        for(int i=1;i<word.length();i++){
+            if(word[i]==word[i-1]){
+                ans++;
+            }
+        }
+        return ans+1;
+    }
+};
