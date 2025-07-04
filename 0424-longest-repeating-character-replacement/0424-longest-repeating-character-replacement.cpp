edb971d535bc58cfbf7854d7d76cf033998c9d32
Prince Choudhury <choudhuryprince19@gmail.com>
2025-07-04 12:47:51 +0530

    Time: 2 ms (75.74%), Space: 10.9 MB (70.49%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,29 +3,20 @@ public:
     int characterReplacement(string s, int k) {
         int maxLen = 0;
         int l = 0;
+        int maxf = 0;
         int freq[26] = {0};
 
         for (int r = 0; r < s.size(); r++) {
             freq[s[r] - 'A']++;
 
-            // Recalculate max frequency after every addition or removal
-            int maxf = 0;
-            for (int i = 0; i < 26; i++) {
-                maxf = max(maxf, freq[i]);
-            }
+            maxf = max(maxf, freq[s[r]-'A']);
 
             int changes = (r-l+1) - maxf;
 
-            // If more than k changes are needed, shrink the window
             while (changes > k) {
                 freq[s[l] - 'A']--;
                 l++;
 
-                // Recalculate maxf again since window changed
-                maxf = 0;
-                for (int i = 0; i < 26; i++) {
-                    maxf = max(maxf, freq[i]);
-                }
                 changes = (r-l+1) - maxf;
             }
 
