a124c5f380df0317a37cd3df7ea3048d724e4d0b
Prince Choudhury <choudhuryprince19@gmail.com>
2025-07-11 23:08:20 +0530

    Time: 89 ms (48.43%), Space: 98.3 MB (47.81%) - LeetHub

---
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        vector<long long> busy(n, 0);
+        vector<int> count(n, 0);
+
+        sort(meetings.begin(), meetings.end());
+
+        for(auto& i : meetings){
+            int start = i[0];
+            int end = i[1];
+            long long earliest = LLONG_MAX;
+            int roomIndex = -1;
+            bool assigned = false;
+
+            for(int i = 0; i < n; i++){
+                if(busy[i] < earliest){
+                    earliest = busy[i];
+                    roomIndex = i;
+                }
+                if(busy[i] <= start){
+                    busy[i] = end;
+                    count[i]++;
+                    assigned = true;
+                    break;
+                }
+            }
+
+            if(!assigned){
+                busy[roomIndex] += (end - start);
+                count[roomIndex]++;
+            }
+        }
+
+        int res = 0;
+        int maxCount = 0;
+        for(int i = 0; i < n; i++){
+            if(count[i] > maxCount){
+                maxCount = count[i];
+                res = i;
+            }
+        }
+        
+        return res;
+    }
+};
